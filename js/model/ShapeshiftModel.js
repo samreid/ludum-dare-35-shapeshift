1c4c6f4f7c7b7325630a2988140c6c5ed11445d5
Jonathan Olson <olsonsjc@gmail.com>
2016-04-16 22:06:25 -0600

    Radial doubling fix

--- js/model/ShapeshiftModel.js
+++ js/model/ShapeshiftModel.js
@@ -19,15 +19,17 @@ define( function( require ) {
   function ShapeshiftModel() {
     var numPoints = Math.floor( Math.random() * 10 ) + 5;
 
-    var array = [ new Vector2( -100, -100 ), new Vector2( 100, -100 ), new Vector2( 100, 100 ), new Vector2( -100, 100 ) ];
+    // var array = [ new Vector2( -100, -100 ), new Vector2( 100, -100 ), new Vector2( 100, 100 ), new Vector2( -100, 100 ) ];
     // var array = [ new Vector2( -100, -100 ), new Vector2( 100, -100 ), new Vector2( -100, 100 ) ];
     // var array = [ new Vector2( -100, -100 ), new Vector2( 100, -100 ), new Vector2( 100, 100 ), new Vector2() ];
-    // var array = [];
-    // for ( var i = 0; i < numPoints; i++ ) {
-    //   var x = Math.random() * 200 - 100;
-    //   var y = Math.random() * 200 - 100;
-    //   array.push( new Vector2( x, y ) );
-    // }
+    var array = [];
+    for ( var i = 0; i < numPoints; i++ ) {
+      array.push( Vector2.createPolar( 50, i * ( Math.PI * 2 ) / numPoints ) );
+      array.push( Vector2.createPolar( 100, ( i + 0.5 ) * ( Math.PI * 2 ) / numPoints ) );
+      // var x = Math.random() * 200 - 100;
+      // var y = Math.random() * 200 - 100;
+      // array.push( new Vector2( x, y ) );
+    }
 
     var initialBody = new Body( array, [] );
 
--- js/model/operations/RadialDoubling.js
+++ js/model/operations/RadialDoubling.js
@@ -24,29 +24,50 @@ define( function( require ) {
 
     apply: function( body ) {
 
+      // closest to 0, in -pi,pi range
+      function mapTheta( angle ) {
+        angle = angle % ( Math.PI * 2 );
+        if ( angle < -Math.PI ) {
+          angle += Math.PI * 2;
+        }
+        if ( angle > Math.PI ) {
+          angle -= Math.PI * 2;
+        }
+        return angle;
+      }
+
       var radialDoubling = function( points ) {
+        var result = [];
+        var i;
+
         var initialTheta = points[ 0 ].angle();
-        var mapFunction = function( angle ) {
-          return function( v ) {
-            var r = v.magnitude();
-            var theta = v.angle();
+        var theta = initialTheta;
+        var mappedTheta = initialTheta;
 
-            var deltaTheta = theta - initialTheta;
+        var firstPoint = points[ 0 ].copy();
+        firstPoint.old = points[ 0 ];
+        result.push( firstPoint );
+        for ( i = 1; i < points.length; i++ ) {
+          var p1Angle = points[ i ].angle();
+          var thetaDelta1 = mapTheta( p1Angle - theta );
+          mappedTheta += thetaDelta1 / 2;
+          theta = p1Angle;
+          var newPoint1 = Vector2.createPolar( points[ i ].magnitude(), mappedTheta );
+          newPoint1.old = points[ i ];
+          result.push( newPoint1 );
+        }
 
-            // while ( deltaTheta > Math.PI ) {
-            //   deltaTheta = deltaTheta - Math.PI * 2;
-            // }
-            // while ( deltaTheta < -Math.PI ) {
-            //   deltaTheta = deltaTheta + Math.PI * 2;
-            // }
+        for ( i = 0; i < points.length; i++ ) {
+          var p2Angle = points[ i ].angle();
+          var thetaDelta2 = mapTheta( p2Angle - theta );
+          mappedTheta += thetaDelta2 / 2;
+          theta = p2Angle;
+          var newPoint2 = Vector2.createPolar( points[ i ].magnitude(), mappedTheta );
+          // no 'old'
+          result.push( newPoint2 );
+        }
 
-            var thetaNew = (deltaTheta) / 2 + angle + initialTheta;
-            var newPoint = Vector2.createPolar( r, thetaNew );
-
-            return newPoint;
-          };
-        };
-        return points.map( mapFunction( 0 ) ).concat( points.map( mapFunction( Math.PI ) ) );
+        return result;
       };
 
       return [
